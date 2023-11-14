41ad828e637f5c93a7d8a82d86ba47729e1aa8db
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-14 18:06:21 -0500

    Updated some of the webpack in webpack.config.js, added the webpack plugin, the css extract plugin, and webpackmanifest

---
+++ README.md
@@ -0,0 +1,4 @@
+# Update History 
+
+- initial commit
+- "Updated some of the webpack in webpack.config.js, added the webpack plugin, the css extract plugin, and webpackmanifest"
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'TODOs',
+        short_name: 'TODOs',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
@@ -27,4 +53,5 @@ module.exports = () => {
       ],
     },
   };
+  
 };
