6eb8baa5e64decee163cdb5b1fad88db1c1f1b5e
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-14 18:13:16 -0500

    As instructed, I added the CSS loaders and babel framework to the webpack.config.js under moldule rules. Did some updates to the webpackmanifest as well.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - initial commit
 - "Updated some of the webpack in webpack.config.js, added the webpack plugin, the css extract plugin, and webpackmanifest"
+- "As instructed, I added the CSS loaders and babel framework to the webpack.config.js under moldule rules. Did some updates to the webpackmanifest as well."
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,15 +30,16 @@ module.exports = () => {
 
       new GenerateSW(),
       new WebpackPwaManifest({
-        name: 'TODOs',
-        short_name: 'TODOs',
-        description: 'Keep track of important tasks!',
+        name: 'Text Editor',
+        short_name: 'TE',
+        description: 'Here you can edit your text!',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
         start_url: './',
         publicPath: './',
         icons: [
           {
+            //Don't know what this is doing yet. 
             src: path.resolve('assets/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
@@ -49,7 +50,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
